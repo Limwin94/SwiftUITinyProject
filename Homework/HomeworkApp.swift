194666adf58cfa6be7b7a1265d7ab93b66f33b28
임승혁 <amuguna94@gmail.com>
2023-06-28 22:12:59 +0900

    ZStack과 Alignment에 대한 예제 입니다. (#1)
    * chore: gitignore 룰 추가
    
    * feature: ZStack과 Alignment를 설정할수 있는 화면 추가
    
    * chore: 파일 그룹핑

---
+++ Homework/HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkApp.swift
+//  Homework
+//
+//  Created by seunghyeok lim on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
